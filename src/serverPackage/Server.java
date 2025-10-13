028ea6fb331f320cf31350e9c96c216e335f034c
aminemedhioub <medaminemedhioub22@gmail.com>
2025-10-13 09:05:11 +0100

    Réalisation de l’Activité 1.2 : communication client-serveur avec service de multiplication

--- src/serverPackage/Server.java
+++ src/serverPackage/Server.java
@@ -1,5 +1,7 @@
 package serverPackage;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -7,15 +9,32 @@ import java.net.Socket;
 public class Server {
     public static void main(String[] args) {
         try {
-            // Création du serveur sur le port 1234
             ServerSocket serverSocket = new ServerSocket(1234);
             System.out.println("attendre la connexion d'un client");
 
             Socket clientSocket = serverSocket.accept();
             System.out.println("client est connecte");
-            clientSocket.close();
-            serverSocket.close();
-            System.out.println("Connexion fermée");
+
+            DataInputStream dis = new DataInputStream(clientSocket.getInputStream());
+            DataOutputStream dos = new DataOutputStream(clientSocket.getOutputStream());
+
+            int x;
+            do {
+                x = dis.readInt();
+                System.out.println("Reçu du client: " + x);
+
+                if (x != 0) {
+                    int result = x * 5;
+                    dos.writeInt(result);
+                    System.out.println("Envoyé au client: " + result);
+                }
+
+            } while (x != 0);
+
+            System.out.println("Client a envoyé 0 ");
+            dis.close();
+            dos.close();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
